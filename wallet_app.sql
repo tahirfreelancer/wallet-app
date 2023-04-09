d90bdbe113d280907fcfecde4ab947aa52e2651b
Shao ch <93768390+itwarehub@users.noreply.github.com>
2023-04-09 20:12:43 +0500

    updated databse file added

--- wallet_app.sql
+++ wallet_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2023 at 05:28 PM
+-- Generation Time: Apr 09, 2023 at 03:12 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.4.20
 
@@ -103,22 +103,61 @@ CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `plans`
+--
+
+DROP TABLE IF EXISTS `plans`;
+CREATE TABLE IF NOT EXISTS `plans` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `amount` int NOT NULL,
+  `gram` int NOT NULL,
+  `estimated_profit` int NOT NULL,
+  `daily_perecntage` int NOT NULL,
+  `bonus` int DEFAULT NULL,
+  `community_bonus` int DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `plans`
+--
+
+INSERT INTO `plans` (`id`, `name`, `amount`, `gram`, `estimated_profit`, `daily_perecntage`, `bonus`, `community_bonus`, `created_at`, `updated_at`) VALUES
+(4, 'last', 500, 5, 30, 10, 23, 200, '2023-04-08 13:07:54', '2023-04-09 08:40:28');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `plan_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `plan_id`, `name`, `email`, `email_verified_at`, `password`, `username`, `phone`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, '1', 'Admin', 'admin@admin.com', NULL, '$2y$10$eQRRQACvx44Y15kRL/6LmePMCvw0X7n1gAqgsdlfGo16t6wGkHV32', 'admin', '', NULL, '2023-04-08 10:50:42', '2023-04-08 10:50:42'),
+(4, '4', 'Tahir Rehman', 'chshao3@gmail.com', NULL, '$2y$10$6GsZ2f4Pcu.guyvegCesYeJ8oROe/b2ztX4SufbfO54AkDbaOK/q6', 'Tah303', '03089101303', NULL, '2023-04-09 10:10:38', '2023-04-09 10:10:38');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
